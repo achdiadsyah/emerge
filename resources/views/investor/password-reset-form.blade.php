aec97f9b6feee0c00d1969c88fdefd2e74bdad84
Faeri Hafriza <faerihafriza@gmail.com>
2022-05-28 21:39:43 +0700

    slice investor auth layout. create or modify investor login, register and forgot password page

--- resources/views/investor/password-reset-form.blade.php
+++ resources/views/investor/password-reset-form.blade.php
@@ -16,15 +16,15 @@
 			<!--begin::Heading-->
 			<div class="mb-10 text-left">
 				<!--begin::Logo-->
-				<img alt="Logo" src="{{ asset('assets/media/logos/emerge-logo.png') }}" class="h-40px" />
+				<!-- <img alt="Logo" src="{{ asset('assets/media/logos/emerge-logo.png') }}" class="h-40px" /> -->
 				<!--end::Logo-->
 			</div>
 			<div class="mb-10 text-left">
 				<!--begin::Link-->
-				<div class="text-dark fw-bold fs-4">Welcome!
+				<div class="text-dark fw-bold fs-4">Reset your password
 				<!--end::Link-->
 				<!--begin::Link-->
-				<div class="text-gray-400 fw-bold fs-4">Sign up and find a great Startup
+				<div class="text-gray-400 fw-bold fs-4">Enter the email address associated with your account, and we'll send you a link to reset your password.
 				<!--end::Link-->
 			</div>
 			<!--end::Heading-->
@@ -34,7 +34,7 @@
 			<!--begin::Input group-->
 			<div class="fv-row mb-7">
 				<label class="form-label fw-bolder text-dark fs-6">Email</label>
-				<input class="form-control form-control-lg form-control-solid" type="email" placeholder="" name="email" autocomplete="off" />
+				<input class="form-control form-control-lg form-control-solid" type="email" placeholder="" name="email" autocomplete="off" disabled />
 			</div>
 			<!--end::Input group-->
 			<!--begin::Input group-->
@@ -63,16 +63,10 @@
 				<input class="form-control form-control-lg form-control-solid" type="password" placeholder="" name="confirm-password" autocomplete="off" />
 			</div>
 			<!--end::Input group-->
-			<!--begin::Input group-->
-			<div class="fv-row mb-7">
-				<label class="form-label fw-bolder text-dark fs-6">Referral Name (Optional)</label>
-				<input class="form-control form-control-lg form-control-solid" type="text" placeholder="" name="referral" autocomplete="off" />
-			</div>
-			<!--end::Input group-->
 			<!--begin::Actions-->
 			<div class="text-center">
 				<button type="button" id="kt_sign_up_submit" class="btn btn-lg btn-primary w-100 mb-5">
-					<span class="indicator-label">Sign Up as Investor</span>
+					<span class="indicator-label">Continue</span>
 					<span class="indicator-progress">Please wait...
 					<span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
 				</button>
@@ -80,18 +74,16 @@
 			<!--end::Actions-->
 		</form>
 		<!--end::Form-->
+		<div class="mb-10 text-center">
+		<!--begin::Login-->
+		<div class="text-gray-400 fw-bold fs-4"><a href="{{ route('investor.login') }}" class="ms-1 link-primary">Return to sign in</a>
+		<!--end::Login-->
+	</div>
 		
 	</div>
 	<!--end::Wrapper-->
 </div>
 <!--end::Content-->
 <!--begin::Footer-->
-<div class="d-flex flex-center flex-wrap fs-6 p-5 pb-0">
-	<div class="mb-10 text-center">
-		<!--begin::Login-->
-		<div class="text-gray-400 fw-bold fs-4">Have an Account? <a href="{{ route('investor.login') }}" class="ms-1 link-primary">Login Now</a>
-		<!--end::Login-->
-	</div>
-</div>
 <!--end::Footer-->
 @endsection
