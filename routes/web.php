fd055d10528108efe5745631632d7f88c4ede4ad
Ryan Syah <achdiadsyah@gmail.com>
2022-05-30 19:29:36 +0700

    Try fixing conflict

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('dashboard');
 
-Route::view('/investor/register', 'investor.register');
+
+Route::prefix('investor')->name('investor.')->group(function(){
+    Route::view('register', 'investor.register',['title' => 'Investor - Register'])->name('register');
+    Route::view('login', 'investor.login', ['title' => 'Investor - Login'])->name('login');
+    Route::view('email-confirmation', 'investor.email-confirmation', ['title' => 'Investor - Email Confirmation'])->name('email-confirmation');
+    Route::view('password-reset-request', 'investor.password-reset-request', ['title' => 'Investor - Password Reset Request'])->name('password-reset-request');
+    Route::view('password-reset-sent', 'investor.password-reset-sent', ['title' => 'Investor - Password Reset Request'])->name('password-reset-sent');
+    Route::view('password-reset-form', 'investor.password-reset-form', ['title' => 'Investor - Password Reset'])->name('password-reset-form');
+    Route::view('password-reset-success', 'investor.password-reset-success', ['title' => 'Investor - Password Reset'])->name('password-reset-success');
+});
 
 Auth::routes();
 
