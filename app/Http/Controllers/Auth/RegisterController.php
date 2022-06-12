0545b56700fd5d6cb4e5c8e484e35419f6e58644
Faeri Hafriza <faerihafriza@gmail.com>
2022-06-12 23:08:53 +0700

    adjust Auth/RegisterController to align the last users table migration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -32,7 +32,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    // protected $redirectTo = RouteServiceProvider::HOME;
 
     /**
      * Create a new controller instance.
@@ -100,7 +100,6 @@ class RegisterController extends Controller
         }
 
         return User::create([
-            'full_name' => 'Tes',
             'role_id' => $role_id,
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
