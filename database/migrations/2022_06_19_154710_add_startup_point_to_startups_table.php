4ece38a66e51237e18fb212f507244295420ad01
Faeri Hafriza <faerihafriza@gmail.com>
2022-06-19 23:14:06 +0700

    create migration add startup_points column to startups table

---
+++ database/migrations/2022_06_19_154710_add_startup_point_to_startups_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('startups', function (Blueprint $table) {
+            $table->integer('startup_point')->after('about_founders');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('startups', function (Blueprint $table) {
+            $table->dropColumn('startup_point');
+        });
+    }
+};
